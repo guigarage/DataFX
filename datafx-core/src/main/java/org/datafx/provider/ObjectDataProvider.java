26db7d1932a2576d4e0e7b501a9286b6eb65e5cd
Johan Vos <johan@lodgon.com>
2013-10-23 17:05:40 +0200

    remove stackdump

--- datafx-core/src/main/java/org/datafx/provider/ObjectDataProvider.java
+++ datafx-core/src/main/java/org/datafx/provider/ObjectDataProvider.java
@@ -207,9 +207,6 @@ public class ObjectDataProvider<T> implements DataProvider<T>, WriteBackProvider
                         T value = null;
                         try {
                             LOGGER.fine("[datafx] get the value of the task");
-                            String tn = Thread.currentThread().getName();
-                            System.out.println("Threadname = "+tn);
-                            Thread.dumpStack();
                             value = task.get();
                             LOGGER.log(Level.FINE, "[datafx] task returned value {0}", value);
                         } catch (InterruptedException e) {
