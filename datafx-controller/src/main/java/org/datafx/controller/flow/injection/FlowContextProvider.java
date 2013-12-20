db7ac06bb4dbd72612de6c54960891c187e25277
Hendrik Ebbers <hendrik.ebbers@web.de>
2013-12-20 19:30:59 +0100

    Refactoring

--- datafx-controller/src/main/java/org/datafx/controller/flow/injection/FlowContextProvider.java
+++ datafx-controller/src/main/java/org/datafx/controller/flow/injection/FlowContextProvider.java
@@ -1,12 +1,13 @@
-package org.datafx.controller.injection.provider;
+package org.datafx.controller.flow.injection;
 
 import org.datafx.controller.context.ApplicationContext;
 import org.datafx.controller.context.ViewContext;
 import org.datafx.controller.flow.context.ViewFlowContext;
 import org.datafx.controller.flow.injection.FlowScoped;
 import org.datafx.controller.injection.ApplicationScoped;
+import org.datafx.controller.injection.provider.ContextProvider;
 
-public class FlowContextProvider implements ContextProvider<FlowScoped, ViewFlowContext>{
+public class FlowContextProvider implements ContextProvider<FlowScoped, ViewFlowContext> {
 
     @Override
     public Class<FlowScoped> supportedAnnotation() {
