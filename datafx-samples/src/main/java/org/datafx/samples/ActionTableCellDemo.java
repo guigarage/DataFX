8fdce0f3564891a1fcdc2c0451d9f34d11e751ad
Hendrik Ebbers <hendrik.ebbers@web.de>
2014-01-30 21:49:05 +0100

    imports

--- datafx-samples/src/main/java/org/datafx/samples/ActionTableCellDemo.java
+++ datafx-samples/src/main/java/org/datafx/samples/ActionTableCellDemo.java
@@ -4,22 +4,12 @@ import javafx.application.Application;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import org.datafx.concurrent.ProcessChain;
 import org.datafx.control.cell.ActionTableCell;
-
-import java.util.function.Consumer;
 
 public class ActionTableCellDemo extends Application {
 
