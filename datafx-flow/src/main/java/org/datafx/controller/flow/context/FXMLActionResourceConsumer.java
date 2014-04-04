928749a9775bdeb4d54ce3ac19c861db64ebb5f9
Hendrik Ebbers <hendrik.ebbers@web.de>
2014-04-04 21:39:19 +0200

    Flow action support for all Node types

--- datafx-flow/src/main/java/org/datafx/controller/flow/context/FXMLActionResourceConsumer.java
+++ datafx-flow/src/main/java/org/datafx/controller/flow/context/FXMLActionResourceConsumer.java
@@ -1,28 +1,28 @@
 package org.datafx.controller.flow.context;
 
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Node;
 import javafx.scene.control.ButtonBase;
+import javafx.scene.control.MenuItem;
 import org.datafx.controller.context.ViewContext;
 import org.datafx.controller.context.resource.ControllerResourceConsumer;
 import org.datafx.controller.flow.action.FXMLFlowAction;
 
-public class FXMLActionResourceConsumer implements ControllerResourceConsumer<FXMLFlowAction, Node>  {
+public class FXMLActionResourceConsumer implements ControllerResourceConsumer<FXMLFlowAction, Object>  {
 
     @Override
-    public void consumeResource(FXMLFlowAction annotation, Node resource, ViewContext<?> context) {
+    public void consumeResource(FXMLFlowAction annotation, Object resource, ViewContext<?> context) {
         FlowActionHandler actionHandler = context.getRegisteredObject(ViewFlowContext.class).getRegisteredObject(FlowActionHandler.class);
         if (resource != null) {
             if (resource instanceof ButtonBase) {
-                ((ButtonBase) resource)
-                        .setOnAction(new EventHandler<ActionEvent>() {
-
-                            @Override
-                            public void handle(ActionEvent event) {
-                                actionHandler.handle(annotation.value());
-                            }
-                        });
+                ((ButtonBase) resource).setOnAction((e) -> actionHandler.handle(annotation.value()));
+            } else if(resource instanceof MenuItem) {
+                ((MenuItem) resource).setOnAction((e) -> actionHandler.handle(annotation.value()));
+            } else if(resource instanceof Node){
+                ((Node) resource).setOnMouseClicked((e) -> {
+                    if(e.getClickCount() > 1) {
+                        actionHandler.handle(annotation.value());
+                    }
+                });
             }
         }
     }
