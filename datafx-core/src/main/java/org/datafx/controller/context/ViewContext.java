c587a8f281abff6ebd1012f28c12d5a02a2d82c6
Hendrik Ebbers <hendrik.ebbers@web.de>
2014-05-14 18:02:17 +0200

    #22 PreDestroy will be called

--- datafx-core/src/main/java/org/datafx/controller/context/ViewContext.java
+++ datafx-core/src/main/java/org/datafx/controller/context/ViewContext.java
@@ -166,11 +166,10 @@ public class ViewContext<U> extends AbstractContext {
     public void destroy() throws IllegalAccessException,
             IllegalArgumentException, InvocationTargetException {
         // TODO: All managed Object should be checked for a pre destroy....
-        Object controller = getRegisteredObject("controller");
         if (controller != null) {
-            for (final Method method : controller.getClass().getDeclaredMethods()) {
+            for (final Method method : getController().getClass().getDeclaredMethods()) {
                 if (method.isAnnotationPresent(PreDestroy.class)) {
-                    method.invoke(controller);
+                    method.invoke(getController());
                 }
             }
         }
