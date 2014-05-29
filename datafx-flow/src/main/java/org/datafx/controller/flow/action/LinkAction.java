89fd1ba537a148c4e99bbed43dc05112e752f0a3
Hendrik Ebbers <hendrik.ebbers@web.de>
2014-05-29 22:35:03 +0200

    JavaDoc

--- datafx-flow/src/main/java/org/datafx/controller/flow/action/LinkAction.java
+++ datafx-flow/src/main/java/org/datafx/controller/flow/action/LinkAction.java
@@ -5,6 +5,15 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Annotation that adds a link action to the annotated node. The action will navigate to another view. The
+ * {@link #value()} field defines the controller class of the target view.
+ * The node value must be injected from fxml. Therefore the LinkAction should be always used in combination with the
+ * {@link javafx.fxml.FXML} annotation.
+ * If a node that extends {@link javafx.scene.control.ButtonBase} or {@link javafx.scene.control.MenuItem} then the back
+ * action will be triggered whenever the node fires an action event. Otherwise the action will be triggered when the
+ * node is clicked.
+ */
 @Target(value={ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface LinkAction {
