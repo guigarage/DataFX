e27cc3f5676e35ff921ce43ed2852390ec0bbb3e
Hendrik Ebbers <hendrik.ebbers@web.de>
2015-01-22 10:54:23 +0100

    layout changes in the sample

--- modules/incubator/samples/src/main/java/io/datafx/samples/eventsystem/EventSystemDemo.java
+++ modules/incubator/samples/src/main/java/io/datafx/samples/eventsystem/EventSystemDemo.java
@@ -3,8 +3,11 @@ package io.datafx.samples.eventsystem;
 import io.datafx.controller.flow.Flow;
 import javafx.application.Application;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 /**
@@ -18,6 +21,8 @@ public class EventSystemDemo extends Application {
         HBox box = new HBox();
         box.setSpacing(12);
         box.setPadding(new Insets(12));
+        box.setFillHeight(true);
+        box.setAlignment(Pos.CENTER);
 
         Flow senderFlow = new Flow(ProducerController.class);
         box.getChildren().add(senderFlow.start());
